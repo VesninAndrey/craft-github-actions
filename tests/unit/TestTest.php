2ce5a360fc159482269695081eb15bbe38e7eb55
Andrey Vesnin <vesninandrey@yandex.ru>
2020-04-27 05:33:31 +0300

    Init

---
+++ tests/unit/TestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests;
+
+use Codeception\Test\Unit;
+
+use Craft;
+use UnitTester;
+
+class TestTest extends Unit
+{
+    /** @var UnitTester */
+    protected $tester;
+
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
